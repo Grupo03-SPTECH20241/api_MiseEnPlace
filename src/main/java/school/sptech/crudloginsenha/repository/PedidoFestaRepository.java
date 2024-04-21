0a1d799162a00583c23e39583f4f0f9fb0f59f37
leandroTokudome <leandro.tokudome@sptech.school>
2024-04-21 14:54:11 -0300

    Criação do classe 'PedidoFesta' e seu ORM com API de CEP

---
+++ src/main/java/school/sptech/crudloginsenha/repository/PedidoFestaRepository.java
@@ -0,0 +1,7 @@
+package school.sptech.crudloginsenha.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import school.sptech.crudloginsenha.entity.PedidoFesta;
+
+public interface PedidoFestaRepository extends JpaRepository<PedidoFesta, Integer> {
+}
