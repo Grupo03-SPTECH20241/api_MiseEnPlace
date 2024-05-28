ac8f43880f38a5d4a567fdae1c5cdda52f234b9a
leandroTokudome <leandro.tokudome@sptech.school>
2024-05-28 18:10:18 -0300

    Removendo a pasta 'crudloginsenha'

--- src/main/java/school/sptech/crudloginsenha/repository/PedidoFestaRepository.java
+++
@@ -1,7 +0,0 @@
-package school.sptech.crudloginsenha.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import school.sptech.crudloginsenha.entity.PedidoFesta;
-
-public interface PedidoFestaRepository extends JpaRepository<PedidoFesta, Integer> {
-}
