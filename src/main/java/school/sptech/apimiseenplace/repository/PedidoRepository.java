1b41cd2e59536028820e09039dc63f02452a8526
leandroTokudome <leandro.tokudome@sptech.school>
2024-06-02 22:39:47 -0300

    Adicionando ordenação de pedidos por data

--- src/main/java/school/sptech/apimiseenplace/repository/PedidoRepository.java
+++ src/main/java/school/sptech/apimiseenplace/repository/PedidoRepository.java
@@ -1,7 +1,12 @@
 package school.sptech.apimiseenplace.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import school.sptech.apimiseenplace.entity.Pedido;
 
+import java.util.List;
+
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+    @Query("SELECT p FROM Pedido p ORDER BY p.dtPedido")
+    List<Pedido> getPedidosEmOrdemDeEntrega();
 }
