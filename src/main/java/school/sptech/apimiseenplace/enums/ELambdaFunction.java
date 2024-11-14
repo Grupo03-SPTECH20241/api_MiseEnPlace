8b94dcde5968c23bc660726683990e4434075865
HenriqueBechis <henrique.coelho@sptech.school>
2024-11-14 19:17:09 -0300

    executav el

--- src/main/java/school/sptech/apimiseenplace/enums/ELambdaFunction.java
+++ src/main/java/school/sptech/apimiseenplace/enums/ELambdaFunction.java
@@ -3,8 +3,8 @@ package school.sptech.apimiseenplace.enums;
 public enum ELambdaFunction {
 
 
-    LAMBDA_FUNCTION_NAME("arn:aws:lambda:us-east-1:532779703639:function:sobeParaS3"),
-    BUCKET_NAME("leandro-bucket-01");
+    LAMBDA_FUNCTION_NAME("arn:aws:lambda:us-east-1:942802636108:function:sobeParaS3"),
+    BUCKET_NAME("bucket-testee");
 
 
     private final String value;
